7237d6ab2960f59da927f5a332bbe4ec2be46c78
roywang1985 <roywang1985@gmail.com>
2016-09-07 10:41:19 +0800

    Update ElasticSearchLogStashEventSerializer.java

--- src/main/java/com/frontier45/flume/sink/elasticsearch2/ElasticSearchLogStashEventSerializer.java
+++ src/main/java/com/frontier45/flume/sink/elasticsearch2/ElasticSearchLogStashEventSerializer.java
@@ -28,6 +28,8 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.google.common.collect.Maps;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
@@ -91,45 +93,70 @@ public class ElasticSearchLogStashEventSerializer implements
     private void appendHeaders(XContentBuilder builder, Event event)
             throws IOException {
         Map<String, String> headers = Maps.newHashMap(event.getHeaders());
+        Set<String> set = new HashSet<>();
 
         String timestamp = headers.get("timestamp");
         if (!StringUtils.isBlank(timestamp)
                 && StringUtils.isBlank(headers.get("@timestamp"))) {
             long timestampMs = Long.parseLong(timestamp);
             builder.field("@timestamp", new Date(timestampMs));
+            set.add("timestamp");
         }
 
         String source = headers.get("source");
         if (!StringUtils.isBlank(source)
                 && StringUtils.isBlank(headers.get("@source"))) {
-            ContentBuilderUtil.appendField(builder, "@source",
-                    source.getBytes(charset));
+//            ContentBuilderUtil.appendField(builder, "@source",source.getBytes(charset));
+            builder.field("@source", source);
+            set.add("source");
         }
 
         String type = headers.get("type");
         if (!StringUtils.isBlank(type)
                 && StringUtils.isBlank(headers.get("@type"))) {
-            ContentBuilderUtil.appendField(builder, "@type", type.getBytes(charset));
+//            ContentBuilderUtil.appendField(builder, "@type", type.getBytes(charset));
+            builder.field("@type", type);
+            set.add("type");
         }
 
         String host = headers.get("host");
         if (!StringUtils.isBlank(host)
                 && StringUtils.isBlank(headers.get("@source_host"))) {
-            ContentBuilderUtil.appendField(builder, "@source_host",
-                    host.getBytes(charset));
+//            ContentBuilderUtil.appendField(builder, "@source_host", host.getBytes(charset));
+            builder.field("@host", host);
+            set.add("host");
         }
 
         String srcPath = headers.get("src_path");
         if (!StringUtils.isBlank(srcPath)
                 && StringUtils.isBlank(headers.get("@source_path"))) {
-            ContentBuilderUtil.appendField(builder, "@source_path",
-                    srcPath.getBytes(charset));
+//            ContentBuilderUtil.appendField(builder, "@source_path", srcPath.getBytes(charset));
+            builder.field("@source_path", srcPath);
+            set.add("src_path");
+        }
+
+        String row = headers.get("row");
+        if (!StringUtils.isBlank(row)
+                && StringUtils.isBlank(headers.get("@row"))) {
+            builder.field("@row", Integer.valueOf(row));
+            set.add("row");
+        }
+
+        String length = headers.get("length");
+        if (!StringUtils.isBlank(length)
+                && StringUtils.isBlank(headers.get("@length"))) {
+            builder.field("@length", Long.valueOf(length));
+            set.add("length");
         }
 
         builder.startObject("@fields");
         for (Map.Entry<String, String> entry : headers.entrySet()) {
-            byte[] val = entry.getValue().getBytes(charset);
-            ContentBuilderUtil.appendField(builder, entry.getKey(), val);
+            if(set.contains(entry.getKey())){
+                continue;
+            }
+//            byte[] val = entry.getValue().getBytes(charset);
+//            ContentBuilderUtil.appendField(builder, entry.getKey(), val);
+            builder.field(entry.getKey(), entry.getValue());
         }
         builder.endObject();
     }
